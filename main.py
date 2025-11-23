8fde26f0ab26cbd6da7493e6ac93a35e5b0ed7df
AndreiGiocas <andrei_giocas@yahoo.com>
2025-11-23 15:11:52 +0200

    Added function to count the words

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+
+def number_of_words(file_contents):
+    split_words = file_contents.split()
+    i = 0
+    for words in split_words:
+        i += 1
+    return i
+
+
 def main():
-    print(get_book_text("/home/shirokets/workspace/github.com/AndreiGiocas/bookbot/bookbot/books/frankenstein.txt"))
+    print(f"Found {number_of_words(get_book_text("/home/shirokets/workspace/github.com/AndreiGiocas/bookbot/bookbot/books/frankenstein.txt"))} total words")
 
 
 main()
