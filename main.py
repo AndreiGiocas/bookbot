9f60ff82b49810ea35bff369f4bab2d817f0b375
AndreiGiocas <andrei_giocas@yahoo.com>
2025-11-24 11:57:18 +0200

    new books and sys added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import number_of_words, num_of_chars, list_of_dict, sort_on
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,12 +7,16 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    #book = "/home/shirokets/workspace/github.com/AndreiGiocas/bookbot/bookbot/books/frankenstein.txt"
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    print(f"Found {number_of_words(get_book_text("/home/shirokets/workspace/github.com/AndreiGiocas/bookbot/bookbot/books/frankenstein.txt"))} total words")
+    print(f"Found {number_of_words(get_book_text(sys.argv[1]))} total words")
     print("--------- Character Count -------")
-    number_of_characters = num_of_chars(get_book_text("/home/shirokets/workspace/github.com/AndreiGiocas/bookbot/bookbot/books/frankenstein.txt"))
+    number_of_characters = num_of_chars(get_book_text(sys.argv[1]))
     list_nline = list_of_dict(number_of_characters)
     for nline in list_nline:
         print(f"{nline["chars"]}: {nline["num"]}")
